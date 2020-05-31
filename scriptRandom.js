1e94801fa8be0ef5b8093ea3faec848f61f22d03
DarkDave <darkdave1185@gmail.com>
2020-05-30 18:03:23 -0700

    having issue getting results to show/ but updating changes

--- scriptRandom.js
+++ scriptRandom.js
@@ -1,10 +1,13 @@
 const resultEl = document.getElementById("result")
-const codeNumberEl = document.getElementById("codeNumber")
+const lengthEl = document.getElementById("length")
 const upperCaseEl = document.getElementById("upperCase")
 const lowerCaseEl = document.getElementById("lowerCase")
 const numberEl = document.getElementById("number")
 const symbolEl = document.getElementById("symbol")
 const generateEl = document.getElementById("generate")
+/*counter and slider input*/
+const rangeNumber = document.getElementsByClassName("rangeNumber")
+const inputNumber = document.getElementsByClassName("inputNumber")
 
 const randomFunction = {
     lower: getRandomLower,
@@ -13,21 +16,17 @@ const randomFunction = {
     symbol: getRandomSymbol
 }
 
-generate.addEventListener("click", ()=> {
-    const codeNumber = +codeNumberEl.value;
+generate.addEventListener("click", ()=>{
     const hasUpperCase = upperCaseEl.checked;
     const hasLowerCase = lowerCaseEl.checked;
     const hasNumber = numberEl.checked;
     const hasSymbol = symbolEl.checked;
+    const length = +lengthEl
 
-    console.log(hasUpperCase, hasLowerCase, hasNumber, hasSymbol)
-
-    resultEl.innerText = generatePassword(
-        hasUpperCase, hasLowerCase, hasNumber, hasSymbol, codeNumber
-    );
+    resultEl.innerText = generatePassword(hasUpperCase, hasLowerCase, hasNumber, hasSymbol, length);
 });
 
-function generatePassword(lower, upper, number, symbol, codeNumber) {
+function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword="";
     const typesCount=lower+upper+number+symbol;
     const typesArr=[{ lower }, { upper }, { number }, { symbol }].filter
@@ -37,16 +36,22 @@ function generatePassword(lower, upper, number, symbol, codeNumber) {
         return "";
     }
     /* loop */
-    for(let i=0; i<codeNumber; i+=typesCount){
-        typesArr.forEach(type => {
+    for(let i=0; i<length; i+=typesCount){
+        typesArr.forEach(type=>{
             const funcName = Object.keys(type)[0];
             generatedPassword += randomFunction[funcName]();
         });
     }
-    /* password created with */
-    const finalPassword=generatedPassword.slice (0, codeNumber);
-        return finalPassword
+/* password created with */
+const finalPassword=generatedPassword.slice (0, length);
+    return finalPassword
 }
+
+const textarea = document.createElement('textarea');
+const password = resultEl.innerText;
+
+textarea.value = password;
+document.body.appendChild(textarea);
 
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random()*26)+65);
@@ -63,4 +68,10 @@ function getRandomNumber() {
 function getRandomSymbol() {
     const symbol = "!@#$%^&*(){}[]=<>/,." 
     return Symbol[Math.floor(Math.random()*symbol.codeNumber)];
+}
+
+function syncCharAmount(e){
+    const value = e.target.value
+    rangeNumber.value = value
+    inputNumber.value = value
 }
